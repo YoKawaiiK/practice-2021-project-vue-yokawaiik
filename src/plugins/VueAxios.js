26e532c609d6bfe3c970147efa8f12ff80ab8b7e
Danil Shubin <danil.legioner@gmail.com>
2021-07-30 19:44:49 +0700

    Добавил axios как плагин, котрый можно использовать глобально в vuex и vue

--- src/plugins/VueAxios.js
+++ src/plugins/VueAxios.js
@@ -1,9 +1,20 @@
 import axios from "axios";
 
-export const VuexAxios = axios;
+// configuration
+let option = {
+  baseURL: "http://89.108.102.170/api/",
+};
+// instance for vuex
+const $axios = axios.create(option);
 
+// export for vuex
+export const VuexAxios = function (store) {
+  store.$axios = $axios;
+};
+// export for vue
 export default {
   install(vue) {
-    vue.prototype.$axios = axios;
+    // vue.prototype.$axios = axios;
+    vue.prototype.$axios = $axios;
   },
 };

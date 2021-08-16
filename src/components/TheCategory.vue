e6e616b8d843e915b0ea5b7bf3efea71d45480bd
Danil Shubin <danil.legioner@gmail.com>
2021-08-16 11:21:39 +0700

    Переделал компонент TheCategory и выделил отдельный компонент-кнопку VAddButtonIcon, так как он встречается еще в таблице транзакций

--- src/components/TheCategory.vue
+++ src/components/TheCategory.vue
@@ -10,14 +10,10 @@
         <h2 class="header_item__title">Summary</h2>
       </div>
       <div class="category_header__header_item">
-        <button class="header_item__button" @click="clickShowModal">
-          <img
-            class="button__icon"
-            src="@/assets/icons/plus-in-circle.svg"
-            alt="plus-in-circle"
-          />
-          <span class="button__text">Add category</span>
-        </button>
+        <v-add-button-icon
+          text="Add category"
+          @click.prevent="clickShowModal"
+        />
       </div>
     </div>
     <section class="category__table_section">
@@ -35,7 +31,7 @@
             :class="{ expense_green: item.category_type === 'income' }"
           >
             <td>{{ item.name }}</td>
-            <td>+999999K</td>
+            <td>+999K</td>
           </tr>
         </tbody>
       </table>
@@ -44,6 +40,8 @@
 </template>
 <script>
 import VModal from "@/components/VModal";
+import VAddButtonIcon from "@/components/VAddButtonIcon";
+
 import { mapGetters, mapActions } from "vuex";
 import {
   CATEGORY,
@@ -56,7 +54,7 @@ import { objectKeysToCamelCase } from "@/utils/index";
 
 export default {
   name: "TheCategory",
-  components: { VModal },
+  components: { VModal, VAddButtonIcon },
   data() {
     return {
       showModal: false,
@@ -86,14 +84,16 @@ export default {
 </script>
 <style lang="scss" scoped>
 .category {
-  padding: 6px calc(26px - 20px) 18px 27px;
+  // padding: 6px calc(26px - 20px) 18px 27px;
+  padding: 6px 26px 18px 27px;
   width: calc(303px - 27px - 6px);
   font-family: $--font-family;
   font-style: normal;
   box-shadow: 1px 8px 25px $--the-category--category--box-shadow;
+  border-radius: 12px;
   .category__category_header {
     display: flex;
-    gap: 50px;
+    gap: calc(50px + 17px);
     .category_header__header_item {
       .header_item__title {
         display: flex;
@@ -105,70 +105,25 @@ export default {
         margin: 0;
         color: $--the-category--text-color;
       }
-      .header_item__button {
-        // margin-left: 50px;
-        padding: 0;
-        display: flex;
-        align-items: center;
-        gap: 16px;
-        color: $--the-category--button--color;
-
-        border: none;
-        background-color: transparent;
-        height: 100%;
-
-        font-size: $--the-category--button--font-size;
-        cursor: pointer;
-        transition: all 0.2s;
-        &:focus {
-          color: $--color-primary-focus;
-        }
-
-        &:active {
-          color: $--text-color-on-primary-light;
-        }
-
-        &:hover {
-          font-weight: $--font-weight-bold;
-          color: $--color-primary;
-        }
-      }
     }
   }
+
   .category__table_section {
     margin-top: 21px;
-    width: 248px;
+    width: calc(248px + 20px);
 
     table {
-      overflow: hidden;
-      width: 100%;
+      border-collapse: collapse;
 
-      > thead {
+      thead {
         display: table;
+        width: 100%;
         table-layout: fixed;
         margin-bottom: 23px;
-        display: table;
-
-        width: 100%;
-        > tr {
-          width: 100%;
-          text-align: left;
-          > th {
-            padding: 0;
-            font-size: $--the-category--text--font-size;
-            line-height: $--the-category--text--line-height;
-            color: $--the-category--text-color;
-            flex: 1;
-
-            &:last-child {
-              padding-left: 57px;
-            }
-          }
-        }
       }
 
       tbody {
-        width: calc(100% + 20px);
+        width: 100%;
         display: block;
         overflow: auto;
         height: 339px;
@@ -177,34 +132,50 @@ export default {
           $--the-category--tbody--linear-gradient 85%,
           transparent 100%
         );
-      }
-      tr {
-        width: 100%;
-        td {
-          width: 50%;
-          font-size: $--the-category--text--font-size;
-          line-height: $--the-category--text--line-height;
-          padding: 0 0px 11px 0px;
-          color: $--the-category--text-color;
 
-          &:first-child {
-            border-right: 1px solid $--the-category--table--border-color;
-          }
-          &:last-child {
-            padding-left: 57px;
-            padding-right: 0;
-          }
+        /* hide scrollbar but allow scrolling */
+        -ms-overflow-style: none; /* for Internet Explorer, Edge */
+        scrollbar-width: none; /* for Firefox */
+        &::-webkit-scrollbar {
+          display: none; /* for Chrome, Safari, and Opera */
         }
-        &:last-child {
+
+        tr {
+          width: 100%;
+          display: table;
+          table-layout: fixed;
+
+          &:last-child {
+            td {
+              padding-bottom: 0px;
+            }
+          }
+
+          &.expense_green {
+            > td {
+              color: $--the-category--text-expense-color;
+            }
+          }
+
           td {
-            padding-bottom: 0;
+            padding: 0 0px 11px 0px;
+            &:first-child {
+              border-right: 1px solid $--the-category--table--border-color;
+            }
           }
         }
       }
-      // green text for expense
-      tr.expense_green {
-        > td {
-          color: $--the-category--text-expense-color;
+
+      th,
+      td {
+        padding: 0;
+        text-align: left;
+        font-size: $--the-category--text--font-size;
+        line-height: $--the-category--text--line-height;
+        color: $--the-category--text-color;
+        &:last-child {
+          padding-left: 57px;
+          padding-right: 5px;
         }
       }
     }
